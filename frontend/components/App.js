b037ac3a6bc6b07440ab0f7ada33df0d6615ca3f
TylerLeachGITgit config --global user.email tylerleach2918@gmail.com <tylerleachgit@gmail.com>
2025-01-28 14:55:14 -0800

    finished

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { Routes, Route, NavLink } from 'react-router-dom'
 import DogForm from './DogForm'
 import DogsList from './DogsList'
 
 export default function App() {
+  const [dogs, setDogs] = useState([])
+  const [currentDogId, setCurrentDogId] = useState(null)
+  
+  useEffect(() => { getDogs() }, [])
+
+  const getDogs = () => {
+    fetch('/api/dogs') //HAD TO RESTART LAPTOP IN ORDER FOR API TO WORK CORRECTLY! RESTART SOONER IF STRANGE PROBLEMS HAPPEN LIKE THAT!
+    .then(res => {
+      if (!res.ok) throw new Error('Problem GETing dogs')
+      return res.json()
+    })
+    .then(setDogs)
+    .catch(err => console.error(err))
+  }
+  
+
   return (
     <div>
       <nav>
@@ -11,8 +27,16 @@ export default function App() {
         <NavLink to="/form">Form</NavLink>
       </nav>
       <Routes>
-        <Route path="/" element={<DogsList />} />
-        <Route path="/form" element={<DogForm />} />
+        <Route path="/" element={<DogsList
+          dogs={dogs}
+          getDogs={getDogs}
+          setCurrentDogId={setCurrentDogId}
+        />} />
+        <Route path="/form" element={<DogForm
+          dog={currentDogId && dogs.find(d => d.id == currentDogId)}
+          getDogs={getDogs}
+          reset={() => setCurrentDogId(null)}
+        />} />
       </Routes>
     </div>
   )
